a34724356f0c47aed2031b9210ccd449c31aaf2e
Mostafa rayan <mr3309296@gmail.com>
2024-09-22 15:43:26 +0300

    final

--- app/_components/ProtectedAuth.jsx
+++ app/_components/ProtectedAuth.jsx
@@ -1,14 +1,17 @@
 "use client";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import { useContext } from "react";
 import { UserContextFromRegisteration } from "../_context/UserContext";
-import { useRouter } from "next/navigation";
-
 const ProtectedAuth = () => {
   const router = useRouter();
   const { user } = useContext(UserContextFromRegisteration);
-  if (user.token == null) {
-    // router.push("/login");
-  }
+  let pathName = usePathname();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+  }, [router, user]);
 };
-
 export default ProtectedAuth;
